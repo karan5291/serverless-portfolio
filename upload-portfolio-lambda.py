6c3377bf987518387f3692bdcf3e3a7bc0085e00
Karl Andersson <karan5291@gmail.com>
2018-04-15 18:26:41 +0200

    Added upload_portfolio-lampda.py

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,19 @@
+import boto3
+import zipfile
+import StringIO
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('portfolio.karl-andersson.net')
+build_bucket = s3.Bucket('portfoliobuild.karl-andersson.net')
+
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfolioBuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj,nm,
+          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
